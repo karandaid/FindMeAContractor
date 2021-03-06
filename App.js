182019d46559d4ca5d6b42c1bb385bf325d159b3
Raja Osama <dks4mii@gmail.com>
2021-03-06 21:00:13 +0500

    Authentication Completed.

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import dva from 'dva';
-import { createMemoryHistory } from 'history';
-import MainNavigator from './src/utils/navigator'
+import {createMemoryHistory} from 'history';
+import MainNavigator from './src/utils/navigator';
+import config from './src/aws-exports';
+import Amplify from 'aws-amplify';
 
+Amplify.configure(config);
 
 //create a dva app instance
 const app = dva({
@@ -14,9 +17,7 @@ const app = dva({
 
 app.model(require('./src/models/app').default);
 //register app router to navigate between views
-app.router(() => (
-  <MainNavigator />
-));
+app.router(() => <MainNavigator />);
 
 //start the app instance and export it to be rendered
 export default app.start();
--- src/utils/navigator.js
+++ src/utils/navigator.js
@@ -2,7 +2,14 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from '../pages/home';
-import {Button, StatusBar, Text, TouchableOpacity, View} from 'react-native';
+import {
+  Alert,
+  Button,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import Job from '../pages/job';
 import Applied from '../pages/applied';
 import Projects from '../pages/projects';
@@ -14,6 +21,12 @@ import Bids from '../pages/bids';
 import Payment from '../pages/payment';
 import Success from '../pages/success';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {withAuthenticator, AmplifyTheme} from 'aws-amplify-react-native';
+import Bootstrap, {Container} from './theme';
+import {Auth} from 'aws-amplify';
+import {Authenticator} from 'aws-amplify-react-native/dist/Auth';
+import theme from './theme';
+
 const Stack = createStackNavigator();
 
 function StackNavigator() {
@@ -119,6 +132,45 @@ const HeaderConfig = ({title, back}) => {
           </TouchableOpacity>
         );
     },
+    headerRight: (e) => {
+      const out = async () => {
+        try {
+          await Auth.signOut();
+        } catch (error) {
+          console.log('Error signing out: ', error);
+        }
+      };
+      return (
+        <TouchableOpacity
+          style={{
+            flexDirection: 'row',
+            alignContent: 'center',
+            justifyContent: 'center',
+          }}
+          onPress={() =>
+            Alert.alert(
+              'Signing Out',
+              'Are you sure you want to sign out.',
+              [
+                {
+                  text: 'Cancel',
+                  onPress: () => console.log('Cancel Pressed'),
+                  style: 'cancel',
+                },
+                {text: 'OK', onPress: out},
+              ],
+              {cancelable: false},
+            )
+          }>
+          <Icon
+            style={{marginRight: 10}}
+            size={20}
+            color={'black'}
+            name={'exit-outline'}
+          />
+        </TouchableOpacity>
+      );
+    },
     headerStyle: {
       elevation: 0, // remove shadow on Android
       shadowOpacity: 0,
@@ -136,4 +188,53 @@ const HeaderConfig = ({title, back}) => {
   };
 };
 
-export default MainNavigator;
+const signUpConfig = {
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: 'Name',
+      key: 'name',
+      required: true,
+      type: 'string',
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      type: 'password',
+    },
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      type: 'string',
+    },
+
+    {
+      label: 'Username',
+      key: 'username',
+      required: true,
+      type: 'string',
+    },
+  ],
+};
+
+const MyTheme = Object.assign({}, AmplifyTheme, {
+  ...theme,
+});
+
+// const App = () => {
+//   return (
+//     <Authenticator signUpConfig={signUpConfig} theme={MyTheme}></Authenticator>
+//   );
+// };
+
+export default withAuthenticator(
+  MainNavigator,
+  {
+    signUpConfig,
+  },
+  [],
+  null,
+  MyTheme,
+);
